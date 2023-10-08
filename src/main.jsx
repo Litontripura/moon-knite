f95858f3a9cf2b6872a2c82f45fc3e88847f90c0
Litontripura <litontripura01@gmail.com>
2023-10-08 09:22:59 +0600

    I have set up all route

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {
-  createBrowserRouter,
+  
   RouterProvider,
 } from "react-router-dom";
+import router from './Components/Router/Routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
